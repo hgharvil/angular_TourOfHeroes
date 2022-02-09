4dc109e2dfb95213ed3467d475fe739f2b3308b8
hgharvil <hg.harvil@gmail.com>
2022-02-09 09:39:45 -0500

    added tutorial items until chapter 4/6

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,11 +10,30 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {id: 1, name:'Windstorm'};
+  heroes: Hero[] = [];
+  // hero: Hero = {id: 1, name:'Windstorm'};
+  selectedHero?: Hero;
 
-  constructor() { }
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {
+
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
 
 }
